7c77bb996309d418279e766bb0f042c68f13e865
Zaeem Atif <67412599+zaeematif@users.noreply.github.com>
2024-07-08 02:41:18 +0300

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
-  "x-rapidapi-key": "75f0cceae2mshda6292dbf4c107bp1667d1jsn9fcc0c3541e9",
+  "x-rapidapi-key": key,
   "x-rapidapi-host": "seeking-alpha.p.rapidapi.com",
 };
 
